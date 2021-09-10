11b5110d76a4250d63a75729a6b3964b82c0f699
Woookeyyy <wilkydolce11@gmail.com>
2021-09-10 14:43:59 -0400

    Mouse Down

--- Pygame/main.py
+++ Pygame/main.py
@@ -1,10 +1,14 @@
-import pygame
+import pygame, random
+
+from pygame import color
 
 pygame.init()
 
 white = (255,255,255)
 black = (0,0,0,0)
-
+red = (255,0,0)
+blue = (0,0,255)
+green =(0,255,0)
 x = 600
 y = 800
 z = [x,y]
@@ -19,13 +23,16 @@ surface = win.set_mode(z)
 python = pygame.image.load('IMG.jpg')
 window = True 
 clock = pygame.time.Clock()
-
+click = 0
+colors = [white,black,red,blue,green]
+fill = white
 
 
 while window:
     font = pygame.font.SysFont('timesnewroman', 20)
     text = font.render('Start', 0, black)
     for event in pygame.event.get():
+        cursor_x, cursor_y = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             window = False
             """
@@ -46,15 +53,26 @@ while window:
                 change_y = 0
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 change_x = 0
-                
+        
 
-    move_x += change_x
+    move_x += change_x 
     move_y += change_y
     """
-    surface.fill(white)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if ((300-cursor_x)**2+(400-cursor_y)**2) < 2500:
+                fill = random.choice(colors)
+
+
+
+
+
+
+    surface.fill(fill)
     #surface.blit(python, (move_x,move_y))
-    surface.blit(text,(300,400))
-    pygame.draw.circle(surface, black, (200,200), 20)
+    surface.blit(text,(x//2-19,y//2-12))
+    pygame.draw.circle(surface, black, (x//2,y//2), 50,1)
+    
+
     win.update()
     clock.tick(30)
 pygame.quit()
