2f8eeba2db34fc2d8811d87ffd8870214e626397
Woookeyyy <wilkydolce11@gmail.com>
2021-09-13 12:57:57 -0400

    #1

---
+++ C_Programming/basic.c
@@ -0,0 +1,11 @@
+
+//Preprocessor: replaces text(starting with #) with the actual content.
+//Stdio.h: standard input output file
+#include <stdio.h>
+
+
+int main()
+{
+    printf("woookeyy");
+    return 0;
+}
